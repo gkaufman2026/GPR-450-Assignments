97248ece300586ece0b7e9ca95632463ce6380c4
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2025-09-02 09:54:24 -0400

    first commit

---
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimationController.c
@@ -0,0 +1,79 @@
+/*
+	Copyright 2011-2025 Daniel S. Buckstein
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+/*
+	animal3D SDK: Minimal 3D Animation Framework
+	By Daniel S. Buckstein
+	
+	a3_KeyframeAnimationController.c
+	Implementation of keyframe animation controller.
+*/
+
+#include "../a3_KeyframeAnimationController.h"
+
+#include <string.h>
+
+
+// macros to help with names
+#define A3_CLIPCTRL_DEFAULTNAME		("unnamed clip ctrl")
+#define A3_CLIPCTRL_SEARCHNAME		((ctrlName && *ctrlName) ? ctrlName : A3_CLIPCTRL_DEFAULTNAME)
+
+
+//-----------------------------------------------------------------------------
+
+// initialize clip controller
+a3i32 a3clipControllerInit(a3_ClipController* clipCtrl_out, const a3byte ctrlName[a3keyframeAnimation_nameLenMax], const a3_ClipPool* clipPool, const a3ui32 clipIndex_pool, const a3i32 playback_step, const a3f64 playback_stepPerSec)
+{
+	a3i32 const ret = a3clipControllerSetClip(clipCtrl_out, clipPool, clipIndex_pool, playback_step, playback_stepPerSec);
+	if (ret >= 0)
+	{
+		strncpy(clipCtrl_out->name, A3_CLIPCTRL_SEARCHNAME, a3keyframeAnimation_nameLenMax);
+		return ret;
+	}
+	return -1;
+}
+
+// update clip controller
+a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
+{
+	if (clipCtrl && clipCtrl->clipPool)
+	{
+//-----------------------------------------------------------------------------
+//****TO-DO-ANIM-PROJECT-1: IMPLEMENT ME
+//-----------------------------------------------------------------------------
+
+		// 1. Time Step: Add it
+		// 2. Resolve Keyframe
+			// a. paused: dt = 0
+			// b. forward: dt > 0
+				//	i. stop
+				//	ii. step(s) taken
+				//  iii: clip exited
+			// c. reverse: dt < 0
+				//  i. stop
+				//  ii. step(s) taken
+				//  iii. clip exited
+		// 3. Normalized keyframe/clip: relative time / duration
+		
+//-----------------------------------------------------------------------------
+//****END-TO-DO-PROJECT-1
+//-----------------------------------------------------------------------------
+	}
+	return -1;
+}
+
+
+//-----------------------------------------------------------------------------
