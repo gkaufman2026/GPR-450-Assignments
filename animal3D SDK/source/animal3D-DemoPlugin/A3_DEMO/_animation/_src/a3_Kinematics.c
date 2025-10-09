023e0f40934e07ccc7ce5f4bbabd368c41eda8f7
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2025-10-09 18:43:47 -0400

    Attempting a bunch of things, unsure at this point

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_Kinematics.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_Kinematics.c
@@ -312,7 +312,7 @@ void a3kinematicsUpdateLookAtIK(a3_HierarchyState const* sceneGraphState,
 	// transform everything into the space of the skeleton/hierarchy
 	// -> look at target
 
-	a3real4x4 transformMat;
+	a3real4x4 transformMat, lookAt;
 	a3real4x4Product(transformMat,
 		baseHS->hpose->hpose_base[hierarchyObjIndex_affected].transformMat.m,
 		sceneGraphState->objectSpace->hpose_base[sceneGraphIndex_effector].transformMat.m);
@@ -327,19 +327,17 @@ void a3kinematicsUpdateLookAtIK(a3_HierarchyState const* sceneGraphState,
 	// Calculating the direction basis by grabbing the effector (target) and the affected (joint)
 	a3vec4 dir;
 	a3real4Diff(&dir.x,
-		&sceneGraphState->localSpace->hpose_base[sceneGraphIndex_effector].transformMat.v3.x, 
-		&baseHS->hpose->hpose_base[hierarchyObjIndex_affected].transformMat.v3.x);
+		&sceneGraphState->objectSpace->hpose_base[sceneGraphIndex_effector].transformMat.v0.x, 
+		&baseHS->hpose->hpose_base[hierarchyObjIndex_affected].transformMat.v0.x);
 
 	a3real4Normalize(&dir.x);
 
 	// adding direction basis to affected matrix
 	m_affected.v0 = dir.xyz;
 
-	a3vec3 vecYZero = { 0, 1, 0 };
-
 	// adding the cross product of the world's up and the direction 
 	// RESULT - SIDE BASIS
-	a3real3Cross(&m_affected.v1.x, &vecYZero.y, &dir.x);
+	//a3real3Cross(&m_affected.v1.x, &m_hierarchyObj.v0.x, &dir.x);
 
 	// adding the cross product of the world's up and the direction 
 	// RESULT - UP BASIS
@@ -348,9 +346,9 @@ void a3kinematicsUpdateLookAtIK(a3_HierarchyState const* sceneGraphState,
 	// Normalizing the direction and the side basis
 	a3real4Normalize(&m_affected.v0.x);
 	a3real4Normalize(&m_affected.v1.x);
-	// DO NOT ADD v2 it will corrupt m_affected!!!
+	//a3real4Normalize(&m_affected.v2.x); 
+	// // DO NOT ADD v2 it will corrupt m_affected!!!
 
-	a3real4x4 lookAt;
 	a3real4x4Set(lookAt, m_affected.v0.x, m_affected.v0.y, m_affected.v0.z, 0,
 						 m_affected.v1.x, m_affected.v1.y, m_affected.v1.z, 0,
 						 m_affected.v2.x, m_affected.v2.y, m_affected.v0.z, 0,
